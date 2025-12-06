36bd4dcfaa6f09c8812f1fc09ada67ab534f6618
Richard Yupanqui <rdyh0906@gmail.com>
2025-12-06 12:41:08 -0300

    Migrating to Minecraft NeoForge 1.21.9

--- src/main/java/com/ryu/minecraft/mod/neoforge/neovillagers/hunter/block/HuntingBlock.java
+++ src/main/java/com/ryu/minecraft/mod/neoforge/neovillagers/hunter/block/HuntingBlock.java
@@ -64,7 +64,7 @@ public class HuntingBlock extends Block {
     
     @Override
     protected InteractionResult useWithoutItem(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, BlockHitResult pHitResult) {
-        if (!pLevel.isClientSide && (pPlayer instanceof ServerPlayer)) {
+        if (!pLevel.isClientSide() && (pPlayer instanceof ServerPlayer)) {
             pPlayer.openMenu(pState.getMenuProvider(pLevel, pPos));
             return InteractionResult.CONSUME;
         }
